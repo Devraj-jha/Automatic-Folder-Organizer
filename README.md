d14a70467346440d93f838144d2930397c70c8a6
Devraj-jha <devrajjha100806@gmail.com>
2025-11-11 10:18:27 +0530

    file utilits done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Automatically sort files in a directory by type (e.g., move all .jpg → “Images” folder)
 The goal of this programme is to give the use ability to path folder. 
 then they can automatically orgainze the files. 
+
+we are trying to break down programms into simple logical chunks so each file does one job. 
--- file_utils.py
+++ file_utils.py
@@ -6,9 +6,7 @@ import shutil
 from categories import CATEGORIES
 
 def get_category(file_name):
-    """
-    🧠 Determines the category (folder name) of a given file based on its extension.
-    """
+   
 
     _ , ext = os.path.splitext(file_name) #. the 2nd line only returns a tuple => ("photo", "jpg")
 # the first line is tuple unpacking python let use unpack the thing. 
@@ -19,3 +17,28 @@ def get_category(file_name):
         if ext in extensions:
             return category
         return "Others"
+    
+
+def organize_folder(folder_path):
+    for item in os.listdir(folder_path):
+        #returns the list of names. in folder path listdire. meaning I guess. 
+        item_path = os.path.join(folder_path, item)
+
+        if os.path.isdir(item_path):
+            continue
+        
+        category = get_category(item)
+
+        category_folder = os.path.join(folder_path, category)
+        
+        if not os.path.exists(category_folder):
+            os.makedirs(category_folder)
+
+        shutil.move(item_path, os.path.join(category_folder, item))
+        print(f"✅ Moved: {item} → {category}/")  # feedback for user
+
+
+def count_files(folder_path):
+   
+    total_files = len([f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))])
+    print(f"\nTotal unorganized files before starting: {total_files}")
